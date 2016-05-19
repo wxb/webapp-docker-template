e2b1485a4f4f8428e153b4a6976557bed0cec73c
wangxb <wxb0328@gmail.com>
2016-05-19 15:33:03 +0800

    first commit

---
+++ Application/Runtime/Cache/0d4eefad9b4f3544c71e02d2e1e7de852c6f9bc9.file.test.html.php
@@ -0,0 +1,31 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2015-12-13 04:23:37
+         compiled from "./Application/Home/View/Index/test.html" */ ?>
+<?php /*%%SmartyHeaderCode:75642595566c6ba5cc6b74-60613271%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0d4eefad9b4f3544c71e02d2e1e7de852c6f9bc9' => 
+    array (
+      0 => './Application/Home/View/Index/test.html',
+      1 => 1449951815,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '75642595566c6ba5cc6b74-60613271',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_566c6ba5d94c4',
+  'variables' => 
+  array (
+    'SMARTY_DEBUG' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_566c6ba5d94c4')) {function content_566c6ba5d94c4($_smarty_tpl) {?>aasftest
+<script type="text/javascript">
+	
+	var SMARTY_DEBUG = <?php echo $_smarty_tpl->tpl_vars['SMARTY_DEBUG']->value;?>
+;
+</script><?php }} ?>
---
+++ Application/Runtime/Cache/Home/5c7a467367f4935f33c107f2984f58a2.php
@@ -0,0 +1,1 @@
+<?php if (!defined('THINK_PATH')) exit();?><script type="text/javascript">var SMARTY_DEBUG = <?php echo ($SMARTY_DEBUG); ?>;</script>
