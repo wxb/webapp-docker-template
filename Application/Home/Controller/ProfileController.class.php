396244c6052073ffe98e844f32a9ec9dd7fa7523
wangxb <wxb0328@gmail.com>
2016-05-22 09:48:42 +0800

    添加.gitignore设置

--- Application/Home/Controller/ProfileController.class.php
+++ Application/Home/Controller/ProfileController.class.php
@@ -1,14 +1,12 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class ProfileController extends Controller {
     public function index(){
-    echo CONTROLLER_NAME;
-    echo ACTION_NAME;
-    die;
     	/*$this->assign('name', 'wangxb')
     		 ->assign('age', 12);
         $this->display();*/
+        echo CONTROLLER_NAME; die;
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
 
--- Application/Runtime/Cache/Home/5c7a467367f4935f33c107f2984f58a2.php
+++
@@ -1,1 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><script type="text/javascript">var SMARTY_DEBUG = <?php echo ($SMARTY_DEBUG); ?>;</script>
